257a8a71a18580c7dd9099ec195aa0b36a7840a5
TY <ty@TYs-MacBook-Pro.local>
2020-05-30 01:50:57 -0500

    call setup.sh from anywhere

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
 
 # setup bashrc
 echo "if tty -s
 then
-    if [ -f ~/.dotfiles/bashrcs/profile ]; then
-        source ~/.dotfiles/bashrcs/profile
+    if [ -f $DIR/bashrcs/profile ]; then
+        source $DIR/bashrcs/profile
     fi
 fi" > ~/.bashrc
 echo "if [ -f "$HOME/.bashrc" ]; then . "$HOME/.bashrc"; fi " > ~/.bash_profile
@@ -12,19 +15,19 @@ echo "if [ -f "$HOME/.bashrc" ]; then . "$HOME/.bashrc"; fi " > ~/.bash_profile
 # setup vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-ln -r -s -f ./my_configs.vim ~/.vim_runtime
+ln -s -f "$DIR/my_configs.vim" ~/.vim_runtime
 
 # setup tmux.conf
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f ~/.tmux/.tmux.conf ~
 cp ~/.tmux/.tmux.conf.local ~
-cat ./vim-tmux-navigator.tmux.conf >> ~/.tmux.conf.local
+cat "$DIR/vim-tmux-navigator.tmux.conf" >> ~/.tmux.conf.local
 
 # get vim plugins
 while IFS=/ read a b
 do
   git clone https://github.com/${a}/${b}.git ~/.vim_runtime/my_plugins/${b}
-done < plugins.list
+done < "$DIR/plugins.list"
 
 # install virtualenvwrapper
 if command -v python3 &>/dev/null; then
